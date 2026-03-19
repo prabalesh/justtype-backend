8192430c27c7d0f7518843127586aec1a797d493
Prabalesh P <prabalesh2008@gmail.com>
2026-03-20 01:29:38 +0530

    feat: Establish CI/CD pipeline for backend deployment to Hetzner, including Nginx and PM2 configurations.

---
+++ ecosystem.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            name: "justtype-backend",
+            script: "./justtype-backend",
+            cwd: "/home/neo/zenko-backend",
+            env: {
+                NODE_ENV: "production",
+            },
+        },
+    ],
+};
